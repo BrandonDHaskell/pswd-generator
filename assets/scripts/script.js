c140955e417e7e00a0ee19b2a92c696a215b085f
Brandon Haskell <bdh747flyna1@hotmail.com>
2022-12-19 17:06:36 -0800

    added character arrays for referencing later

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,7 @@
-// Assignment code here
+var specChars = [ '\u0021', '\u201D', '\u0023', '\u0024', '\u0025', '\u0026', '\u2019', '\u0028', '\u0029', '\u002A', '\u002B', '\u002C', '\u002D', '\u002E', '\u002F', '\u003A', '\u003B', '\u003C', '\u003D', '\u003E', '\u003F', '\u0040', '\u005B', '\u005C', '\u005D', '\u005E', '\u005F', '\u0060', '\u007B', '\u007C', '\u007D', '\u007E' ]
+var numChars = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+var capChars = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]
+var lwrChars = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
 
 
 // Get references to the #generate element
